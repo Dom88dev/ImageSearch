52edfd9a30d03e44b27a68dee685551a3afe56f5
DomDev <dom88.dev@gmail.com>
2021-10-17 10:04:28 +0900

    fix - 목록 아이템간 여백 조절 수정

--- app/src/main/java/dom/project/imagesearch/view/adapter/ImageItemDecoration.kt
+++ app/src/main/java/dom/project/imagesearch/view/adapter/ImageItemDecoration.kt
@@ -2,6 +2,7 @@ package dom.project.imagesearch.view.adapter
 
 import android.content.Context
 import android.graphics.Rect
+import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
@@ -18,25 +19,25 @@ class ImageItemDecoration : RecyclerView.ItemDecoration() {
         super.getItemOffsets(outRect, view, parent, state)
 
         val position = parent.getChildAdapterPosition(view)
-        val size5 = dpToPx(parent.context, 5f)
 
-        if (position < 3) {
-            outRect.top = size5
-        }
+        val size3 = dpToPx(parent.context, 3f)
+        val size4 = dpToPx(parent.context, 4f)
+        val size2 = dpToPx(parent.context, 2f)
 
         when (position % 3) {
+            0 -> {
+                outRect.right = size4
+            }
             1 -> {
-                outRect.left = size5
+                outRect.left = size2
+                outRect.right = size2
             }
             2 -> {
-                outRect.left = size5
-                outRect.right = size5
-            }
-            0 -> {
-                outRect.right = size5
+                outRect.left = size4
             }
         }
-        outRect.bottom = size5
+        outRect.top = size3
+        outRect.bottom = size3
     }
 
     private fun dpToPx(context: Context, dp: Float): Int {
